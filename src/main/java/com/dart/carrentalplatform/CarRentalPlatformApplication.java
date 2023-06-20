074a536766b1d74dbf6bc5eeed03aad1e497942b
Founchoo <founchoo@outlook.com>
2023-06-20 16:52:19 +0800

    2023年6月20日 16点52分

---
+++ src/main/java/com/dart/carrentalplatform/CarRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dart.carrentalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalPlatformApplication.class, args);
+    }
+
+}
